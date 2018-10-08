861bfd940144c90f8ae89736d74429e842de4efa
Andrew Abbott <andrew.abbott@reason.org>
2018-10-08 15:36:35 -0500

    Two graph tabs added

--- app.R
+++ app.R
@@ -13,6 +13,7 @@ library(RPostgres)
 library(tidyverse)
 library(dbplyr)
 library(DT)
+library(plotly)
 
 # The folliwing url is provided by Heroku
 url <- "postgres://viliygpvlizwel:5c26e3ddd0b2682b5c71a4230547677007d7f9fcfe1ed1c29ee45d6375a7475d@ec2-54-235-177-45.compute-1.amazonaws.com:5432/d47an5cjnv5mjb"
@@ -52,7 +53,7 @@ inner join data_source
 on plan_attribute.data_source_id = data_source.id
   where cast(plan_annual_attribute.year as integer) >= 1980 and
 data_source_id <> 1 and
-  plan_id = "
+  plan_id ="
 
 # create a connection from the url using the parsed pieces
 con <- dbConnect(RPostgres::Postgres(),
@@ -99,8 +100,12 @@ ui <- fluidPage(
       mainPanel(
         tabsetPanel(
           type = "tabs", 
-          tabPanel("Plot #1", 
-                   #plotlyOutput(outputId = "p"), 
+          tabPanel("Contributions", 
+                   plotOutput(outputId = "p2"), 
+                   br()
+          ),
+          tabPanel("Mountain of Debt", 
+                   plotlyOutput(outputId = "p"), 
                    br()
                    ),
           tabPanel("Data", 
@@ -120,6 +125,11 @@ server <- function(input, output) {
     planList %>% subset(state == input$state) %>% select(display_name)
   })
   
+  query <- reactive({
+    planList$id[planList$display_name == input$plan]
+    paste0(q2, planList$id[planList$display_name == input$plan]," order by year, state, plan.display_name")
+  })
+  
   dataset <- reactive({
     con <- dbConnect(RPostgres::Postgres(),
                      dbname = trimws(pg$path),
@@ -129,7 +139,7 @@ server <- function(input, output) {
                      password = pg$password,
                      sslmode = "require"
     )
-    res2 <- dbSendQuery(con, paste0(q2, planList$id[planList$display_name == input$plan]," order by year, state, plan.display_name"))
+    res2 <- dbSendQuery(con, query())
     subset_data <- dbFetch(res2)
     dbClearResult(res2)
     dbDisconnect(con)
@@ -138,8 +148,24 @@ server <- function(input, output) {
       spread(attribute_name, attribute_value) 
   })
   
+  selectedData <- reactive({
+    dataset() %>% 
+      select(year, display_name, `Actuarial Assets under GASB standards`, `Actuarial Accrued Liabilities Under GASB Standards`, 
+             `Employer Annual Required Contribution`, `Employer Contributions`, `Covered Payroll`) %>% 
+      rename(actuarialAssets = `Actuarial Assets under GASB standards`, 
+             AAL = `Actuarial Accrued Liabilities Under GASB Standards`, 
+             ADEC = `Employer Annual Required Contribution`,
+             empCont = `Employer Contributions`,
+             payroll = `Covered Payroll`) %>%
+      mutate(UAAL = as.numeric(AAL) - as.numeric(actuarialAssets), 
+             fundedRatio = as.numeric(actuarialAssets) / as.numeric(AAL), 
+             `ADEC Contribution Rates` = 100*as.numeric(ADEC)/as.numeric(payroll),
+             `Actual Contribution Rates (Statutory)` = 100*as.numeric(empCont)/as.numeric(payroll)
+      )
+  })
+  
   output$plan <- renderUI({
-    selectInput("plan", "Select a plan:", choices = plans())
+    selectInput("plan", "Select a plan:", choices = plans(), selected = "Texas Employees Retirement System")
   })
   
   output$text <- renderUI({
@@ -147,15 +173,108 @@ server <- function(input, output) {
   })
   
   output$table <- renderDT({
-    dataset()
+    selectedData()
   })
   
   output$download_data <- downloadHandler(
     filename = "data.csv",
     content = function(file) {
-      write.csv(dataset(), file)
+#      write.csv(dataset(), file)
+      write.csv(debt(), file)
     }
   )
+  
+  output$p <- renderPlotly({
+    df <- selectedData()
+    #extrapolate data points to "smooth out" the area chart 
+    df$year <- as.numeric(as.character(df$year))
+    df$fundedRatio <- df$fundedRatio*100
+    extrapo <- approx(df$year, df$UAAL, n = 10000)
+    extrapo2 <- approx(df$year, df$fundedRatio, n = 10000)
+    graph <- data.frame(year = extrapo$x, UAAL = extrapo$y, fundedRatio = extrapo2$y)
+    
+    #create a "negative-positive" column for fill aesthetic
+    graph$sign[graph$UAAL >= 0] = "positive"
+    graph$sign[graph$UAAL < 0] = "negative"
+    cols <- c("positive" = "#CC0000", "negative" = "#669900")
+    
+    p <- ggplot(graph, aes(x = year)) +
+      geom_area(aes(y = UAAL, fill = sign), show.legend = FALSE) +
+      scale_fill_manual(values = cols) +
+      geom_line(aes(y = UAAL)) +
+      geom_line(aes(y = fundedRatio), color = '#3300FF', size = 1) +
+      labs(y = 'Unfunded Accrued Actuarial Liabilities', x = NULL) +
+      theme(
+        axis.line.y = element_line(color = "black"),
+        axis.text.x = element_text(
+          face = "bold",
+          size = 14,
+          hjust = 1,
+          angle = 90,
+          color = "black"
+        ),
+        axis.title.x = element_blank(),
+        axis.title.y.left = element_text(face = 'bold', size = 14, color = "black"),
+        axis.text.y.left = element_text(face = "bold", size = 14, color = "black"),
+        axis.title.y.right = element_text(face = 'bold', size = 14, color = "black"),
+        axis.text.y.right = element_text(face = "bold", size = 14, color = "black"),
+        panel.background = element_blank(),
+        legend.position = "none"
+      ) +
+      scale_y_continuous(
+        breaks = round(seq(min(graph$UAAL), max(graph$UAAL), by = 1000000), -3),
+        labels = dollar_format(prefix = "$"),
+        sec.axis = sec_axis(
+          ~ . / (max(graph$UAAL) / 100),
+          breaks = seq(-10, 135, 10),
+          name = "Funded Ratio",
+          labels = function(b) {
+            paste0(round(b, 0), "%")
+          }
+        )
+      ) +
+      scale_x_continuous(breaks = round(seq(min(graph$year), max(graph$year), by = 2), 1)) +
+      geom_hline(yintercept = 0, color = "black") +
+      geom_line(aes(
+        y = fundedRatio * (max(graph$UAAL) / 100)),
+        color = '#3300FF',
+        size = 1
+      )
+    ggplotly(p)
+  })
+  
+  output$p2 <- renderPlot({
+    df <- selectedData() %>% 
+      select(year, `ADEC Contribution Rates`, `Actual Contribution Rates (Statutory)`) %>% 
+      mutate_all(funs(as.numeric)) %>% 
+      gather(key = contribution, value = amount, -year)
+    p2 <- ggplot(df, aes(x = year)) +
+      geom_line(aes(y = amount, color = contribution)) +
+      theme(
+        axis.line = element_line(color = "black"),
+        axis.text.x = element_text(
+          face = "bold",
+          size = 14,
+          hjust = 1,
+          angle = 90,
+          color = "black"
+        ),
+        axis.title.x = element_blank(),
+        axis.title.y.left = element_text(face = 'bold', size = 14, color = "black"),
+        axis.text.y.left = element_text(face = "bold", size = 14, color = "black"),
+        panel.background = element_blank(),
+        legend.title = element_blank(),
+        legend.text = element_text(face = "bold", size = 14, color = "black"),
+        legend.position = c(0.3, 0.8)
+      ) +
+      labs(y = "Employer Contribution (% of Payroll)") +
+      scale_y_continuous(
+        breaks = round(seq(0, max(df$amount), by = 2), 1),
+        labels = percent_format(accuracy = 0.1, scale = 1)
+      ) +
+      scale_x_continuous(breaks = round(seq(min(df$year), max(df$year), by = 2), 1))
+    p2
+  })
   
 }
 
