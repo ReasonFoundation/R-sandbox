8ba8f61e1ca02961b68260dce456bdfbb44a7354
anabbott <anabbott@smu.edu>
2019-01-08 12:04:03 -0600

    added janitor::clean_names() to clean up variable names.

--- fundingModelLib.R
+++ fundingModelLib.R
@@ -1,3 +1,55 @@
+####################################################################
+# Description: This function selects the data used in the funding model
+# Parameters:
+#     wideData = a datasource in wide format
+#     dateCol = column name for valuation date. Default: 'Actuarial Valuation Date For GASB Assumptions',
+#     aalCol = column name AAL. Default: 'Actuarial Accrued Liabilities Under GASB Standards',
+#     assetCol = column name for Actuarial Assets. Default: 'Actuarial Assets under GASB standards',
+#     ADECCol = column name for ADEC. Default: 'Employer Annual Required Contribution',
+#     empContCol = column name for employer contributions. Default: 'Employer Contributions',
+#     payrollCol = column name for payroll. Default: 'Covered Payroll'
+# Usage: data <- selected_Data(wideData,
+#                   dateCol = 'Actuarial Valuation Date For GASB Assumptions',
+#                   aalCol = 'Actuarial Accrued Liabilities Under GASB Standards',
+#                   assetCol = 'Actuarial Assets under GASB standards',
+#                   ADECCol = 'Employer Annual Required Contribution',
+#                   empContCol = 'Employer Contributions',
+#                   payrollCol = 'Covered Payroll')
+
+
+fundingData <- function(wide_data,
+                        date_col = "actuarial_valuation_date_for_gasb_assumptions",
+                        aal_col = "actuarial_accrued_liabilities_under_gasb_standards",
+                        asset_col = "actuarial_assets_under_gasb_standards",
+                        adec_col = "employer_annual_required_contribution",
+                        emp_cont_col = "employer_contributions",
+                        payroll_col = "covered_payroll") {
+  require(tidyverse)
+  require(lubridate)
+  require(janitor)
+  
+  wide_data %>%
+    mutate(
+      year = year(excel_numeric_to_date(as.numeric(actuarial_valuation_date_for_gasb_assumptions))),
+      valuation_date = excel_numeric_to_date(as.numeric(actuarial_valuation_date_for_gasb_assumptions))
+    ) %>%
+    select(
+      year,
+      valuation_date,
+      actuarial_assets = asset_col,
+      aal = aal_col,
+      adec = adec_col,
+      emp_cont = emp_cont_col,
+      payroll = payroll_col
+    ) %>%
+    mutate(
+      uaal = as.numeric(aal) - as.numeric(actuarial_assets),
+      funded_ratio = as.numeric(actuarial_assets) / as.numeric(aal),
+      adec_contribution_rates = as.numeric(adec) / as.numeric(payroll),
+      actual_contribution_rates = as.numeric(emp_cont) / as.numeric(payroll)
+    ) %>%
+    drop_na()
+}
 
 ####################################################################
 # Description: This function projects payroll at the payroll growth rate
