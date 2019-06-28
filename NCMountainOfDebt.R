2961d74f98686e8c38dfcf8ee0ca0dde9b456a03
anabbott <anabbott@smu.edu>
2019-06-28 17:58:59 -0500

    trying out github pages

--- NCMountainOfDebt.R
+++ NCMountainOfDebt.R
@@ -1,21 +1,11 @@
-require(xts)
-require(tidyverse)
-require(lubridate)
-options(viewer = NULL)
-source("pensionFuns.R")
-
-installRequiredPackages()
-allWide <- loadData('NorthCarolina_PensionDatabase_TSERS.xlsx') %>% 
+data <- loadData('data/NorthCarolina_PensionDatabase_TSERS.xlsx') %>% 
   select('fiscal_year_end', 'actuarial_accrued_liability', 'actuarial_value_of_assets') %>% 
-  drop_na()
+  drop_na() %>% 
+  select(year = fiscal_year_end, aal = actuarial_accrued_liability, actuarial_assets = actuarial_value_of_assets) %>% 
+  mutate(
+    uaal = as.numeric(aal) - as.numeric(actuarial_assets),
+    funded_ratio = as.numeric(actuarial_assets) / as.numeric(aal)
+    )
 
-data <- modData(allWide,
-                year_col = 'fiscal_year_end',
-                aal_col = 'actuarial_accrued_liability',
-                asset_col = 'actuarial_value_of_assets',
-                base = 1)
-
-modGraph(data)
-
-modTable(data)
-
+NC_debt_plot <- debtPlot(data)
+NC_debt_plot
--- odbcdatabasePull.R
+++ odbcdatabasePull.R
@@ -2,9 +2,12 @@ library(RPostgres)
 library(tidyverse)
 library(dbplyr)
 library(janitor)
+library(DBI)
+library(odbc)
 
 # The folliwing url is provided by Heroku
-url <- "postgres://viliygpvlizwel:5c26e3ddd0b2682b5c71a4230547677007d7f9fcfe1ed1c29ee45d6375a7475d@ec2-54-235-177-45.compute-1.amazonaws.com:5432/d47an5cjnv5mjb"
+url <- "postgres://u9a2ef5lju8rrc:p54e096881444359c1c6cf3992281aaeecbe8f2bd5aa5e993f4811c2e7316cfb2@ec2-3-209-200-73.compute-1.amazonaws.com:5432/d629vjn37pbl3l"
+
 
 # To parse the url into usable sections use parse_url
 pg <- httr::parse_url(url)
@@ -24,23 +27,25 @@ q2 <- "select plan_annual_master_attribute.year,
   on plan_annual_master_attribute.master_attribute_id = plan_master_attribute_names.id
    order by year, plan_annual_master_attribute.plan_id, plan_master_attribute_names.id"
 # create a connection from the url using the parsed pieces
-con <- dbConnect(RPostgres::Postgres(),
-                          dbname = trimws(pg$path),
-                          host = pg$hostname,
-                          port = pg$port,
-                          user = pg$username,
-                          password = pg$password,
+db_con <- dbConnect(odbc::odbc(),
+                          driver = "PostgreSQL Driver",
+                          database = trimws(pg$path),
+                          server = pg$hostname,
+                          Port = pg$port,
+                          uid = pg$username,
+                          pwd = pg$password,
                           sslmode = "require"
                  )
 
 
 # Run a SQL query 
-res <- dbSendQuery(con,q2)
+res <- dbSendQuery(db_con,q2)
 allData <- dbFetch(res)
 dbClearResult(res)
 dbDisconnect(con)
 allWide <- allData %>% 
-  filter(display_name == 'Texas Teacher Retirement System') 
+  filter(State == "Mississippi")
+ # filter(display_name == 'Mississippi Public Employees Retirement System') 
 allWide <- allWide %>% 
   select(year, plan_id, display_name, name, attribute_value) %>% 
   spread(name, attribute_value) 
